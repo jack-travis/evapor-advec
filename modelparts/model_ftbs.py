4d973c3223e5dc6cf823f0b1d7bc8764a5434fb5
jack-travis <jack_travis_@hotmail.com>
2018-04-26 11:45:59 +0100

    Updated ftbs

--- modelparts/model_ftbs.py
+++ modelparts/model_ftbs.py
@@ -1,7 +1,7 @@
 import numpy
-from matplotlib import pyplot
 
 class core:
+    #need to "const"ify
     def __init__(self):
         #parameters
         self.L = 1.0
@@ -15,15 +15,24 @@ class core:
         self.liquid = numpy.array([0.0]*self.gs)
         self.E = numpy.array([0.0]*self.gs)
         self.t = 0.0
+        self.initial = [0.0,0.0]
+    def initialise(self):
+        self.Dx = self.L / self.gs
+        self.initial = [self.vapour[0],self.liquid[0]]
     def step(self):
+        if self.t <= 0.0:
+            self.initialise()
+        #handle i>0
+        v_n = self.vapour.copy()
+        l_n = self.liquid.copy()
         for i in range(self.gs):
             E_n = self.E[i]
-            if E_n > 0.0 and self.liquid[i] <= 0.0:
+            if E_n > 0.0 and l_n[i] <= 0.0:
                 E_n = 0.0
-            elif self.vapour[i] <= 0.0:
+            if E_n < 0.0 and v_n[i] <= 0.0:
                 E_n = 0.0
-            drv = self.vapour[i] - self.vapour[i-1]
-            drl = self.liquid[i] - self.liquid[i-1]
+            drv = v_n[i] - (v_n[i-1] if i > 0 else self.initial[0])
+            drl = l_n[i] - (l_n[i-1] if i > 0 else self.initial[1])
             self.vapour[i] += self.Dt*(+E_n - (self.u/self.Dx)*drv)
             self.liquid[i] += self.Dt*(-E_n - (self.u/self.Dx)*drl)
         self.t += self.Dt
@@ -31,24 +40,4 @@ class core:
         steps = int(float(duration) / self.Dt)
         for _ in range(steps):
             self.step()
-
-du = [1.0,0.8,0.6,0.4,0.2,0.0]
-for i in range(len(du)):
-    c = core()
-    #initial conditions
-    c.u *= du[i]
-    #
-    c.vapour[:] = 1.0
-    c.liquid[:] = 0.0
-    c.E[:] = -0.5/c.Dt
-    c.E[c.gs/3:2*c.gs/3] = 0.5/c.Dt
-    #
-    c.run(100)
-    #
-    X = numpy.linspace(0.0,c.L,c.gs)
-    pyplot.subplot(2,3,i+1)
-    pyplot.title("u={0}".format(c.u))
-    pyplot.plot(X,c.vapour,label="Water vapour")
-    pyplot.plot(X,c.liquid,label="Liquid water")
-    pyplot.legend()
-pyplot.show(block=False)
+#
